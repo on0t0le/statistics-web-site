ef35dc7afd6c9d95104277026b34e0b13049c7ae
unknown <bugrovka12@ukr.net>
2019-05-12 15:29:37 +0300

    Form no date validation

--- src/app/get-stat.service.ts
+++ src/app/get-stat.service.ts
@@ -9,7 +9,13 @@ export class GetStatService {
   BASE_URL = 'http://192.168.88.100:3000'
   constructor(private http: HttpClient) { }
 
-  public getStatistics(){
-    return this.http.get(this.BASE_URL+'/api/calls');
+  public getStatistics(_startDate: string,_operator: string){
+    let body = {
+      startDate:_startDate,
+	    endDate:"2019-05-10",
+	    operator:_operator,
+	    status:"BUSY"
+    }
+    return this.http.post(this.BASE_URL+'/api/calls',body);
   }
 }
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { DatePipe } from '@angular/common';
-import { FormControl } from '@angular/forms';
 import { GetStatService } from '../get-stat.service';
 
 @Component({
@@ -13,18 +12,21 @@ export class HomePageComponent {
   constructor(private datePipe: DatePipe, private statService: GetStatService) { }
 
   startDate: Date = new Date(Date.now());
-  selectedOperator: any;
-  operatorsList: string[] = ['1206','1207','1238'];
+  endDate: Date = new Date(Date.now());
 
-  getDate(){
-    let tmpDate = this.datePipe.transform(this.startDate,'yyyy-MM-dd')
-    console.log('You choose: ', tmpDate);    
-    console.log('You chose such operators: ',this.selectedOperator);
-    
+  selectedOperator: string = 'all';
+  operatorsList: string[] = ['all','1206','1207','1238'];
+
+  selectedStatus: string = 'all';
+  statusList = statuses;
+
+  testForm(){
+    console.log('You choose: start date '+this.startDate+', end date '+this.endDate+', operator '+this.selectedOperator+', status '+this.selectedStatus); 
   }
 
   getData(){
-    this.statService.getStatistics().subscribe(data =>{
+    let _startDate = this.datePipe.transform(this.startDate,'yyyy-MM-dd')
+    this.statService.getStatistics(_startDate,this.selectedOperator).subscribe(data =>{
       console.log('Get this data: ',data);
       
     },
@@ -34,3 +36,11 @@ export class HomePageComponent {
   }
   
 }
+
+
+const statuses = [
+  {id: 'all', name:'Everything'},
+  {id: 'ANSWERED', name:'Answered'},
+  {id: 'BUSY', name:'Busy'},
+  {id: 'NO ANSWER', name:'Not answered'}
+]
