92a328c42025b2404c0f4e98db0251a606c44824
unknown <bugrovka12@ukr.net>
2019-05-15 16:26:50 +0300

    Add details to row on test route

--- src/app/test-page/test-page.component.ts
+++ src/app/test-page/test-page.component.ts
@@ -1,47 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { GetStatService } from '../get-stat.service';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-test-page',
   templateUrl: './test-page.component.html',
-  styleUrls: ['./test-page.component.css']
+  styleUrls: ['./test-page.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0', display: 'none'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ]
 })
 export class TestPageComponent implements OnInit {
+  statisticsDataSource: any;
 
-  selectedStartDate: Date = new Date(Date.now());
-  selectedEndDate: Date = new Date(Date.now());
+  //Availible: start, disposition, diff, userfield
+  displayedColumns = ['src','dstchannel'];
+
+  expandedElement: any | null;
 
   constructor(private callsService: GetStatService) { }
 
   ngOnInit() {
   }
 
-  getCalls(){
+  getCalls() {
     let sd = '2019-05-12'
     let ed = '2019-05-12'
     let op = '1250'
     let st = 'BUSY'
 
-    this.callsService.getStatistics(sd,ed,op,st).subscribe((data: any) =>{      
-      console.log(data);      
+    this.callsService.getStatistics(sd, ed, op, st).subscribe((data: any) => {
+      console.log(data);
+      this.statisticsDataSource = data;
     },
-    err =>{
-      console.error(err);
-    })
-  }
-
-  startFilter = (d: Date): boolean => {
-    if (d > this.selectedEndDate || d < new Date('2016-08-19')) {
-      return false
-    }
-    return true;    
-  }
-  
-  endFilter = (d: Date): boolean => {
-    let date = d.getDate()
-    if (date > (new Date(Date.now())).getDate() || date < this.selectedStartDate.getDate()) {
-      return false
-    }
-    return true;    
+      err => {
+        console.error(err);
+      })
   }
 }
